a76ba8be90859485a02ee167a0e6f361d9e8126b
User <user@strontium.(none)>
2018-06-18 13:25:09 +0200

    Approxikit files

---
+++ APPROXIKIT/approxikit.h
@@ -0,0 +1,55 @@
+#define MAX_THREAD 65
+int pertubations;
+int perturbed_bytes;
+
+void approxikit_init()
+{
+    pertubations = 0;
+    perturbed_bytes = 0;
+}
+
+void approxikit_status()
+{
+    printf("APPROXIKIT total pertubations = %d\n",pertubations);
+    printf("APPROXIKIT total bytes pertubed = %d\n",perturbed_bytes);
+}
+
+char BitFlip(char v, int b)
+{
+    printf("\nAPPROXIKIT: AZZACCAGNAI!");
+      int bit_b = v & (1 << b);
+        if (bit_b == 0)
+	        return v | (1 << b);
+	  else
+	          return v & ~(1 << b);
+}
+// ---------------------------------------------------------------------------
+
+void Perturb(void *addr, unsigned int size, double ber)
+{
+    //printf("APPROXIKIT: Pertubating address %x, size %d, ber %lf\n",addr,size,ber);
+    pertubations++;
+    char *data = (char*)addr;
+
+    for (int i=0; i<size; i++)
+    {
+	if ((double)rand() / RAND_MAX <= ber*8)
+	    *(data+i) = BitFlip(*(data+i), rand() % 8); // flip a random bit in a byte
+    }
+
+    perturbed_bytes+=size;
+}
+// ---------------------------------------------------------------------------
+void annotate_address_home(char * label,long unsigned int address, int size)
+{
+    FILE * fp;
+    printf("writing annotated regions\n");
+    fp = fopen ("/sim/graphite/results/latest/APPROXIKIT_annotated_regions.txt", "w+");
+    int granularity = 1;
+    int n_controllers = 2;
+    int id = (address>>granularity)%n_controllers;
+    //printf("[DATA_LABEL: %s],  ahl %d, n_controllers %d, ADDRESS %lx, TILE ID %d (+%d bytes)\n", label,granularity,n_controllers,A,id,length);
+    fprintf(fp,"LABEL:%s g:%d nc:%d addr:%lx size:%d\n",label,granularity,n_controllers,address,size);
+    printf("Annotating region [LABEL:%s](%lx-%lx,%d)\n",label,address,address+size,size);
+    fclose(fp);
+}
---
+++ APPROXIKIT/approxiscanner.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#define MAX_REGIONS 10
+#define MAX_THREADS 65
+#define NOT_VALID 0
+
+typedef struct {
+    long unsigned address;
+    int size;
+    char label[16];
+} Region;
+
+int communications[MAX_THREADS][MAX_THREADS];
+
+Region regions[MAX_REGIONS];
+
+int main(int argc, char *argv[])
+{
+    int bufSize = 128;
+    char label[50];
+    int granularity,n_controllers,size,src,dst; 
+    long unsigned address;
+
+    int current_region = 0;
+    int total_regions = 0;
+
+    int i,j;
+
+    for (i=0;i<MAX_THREADS;i++)
+        for(j=0;j<MAX_THREADS;j++)
+            communications[i][j]=0;
+
+    for (i=0;i<MAX_REGIONS;i++)
+    {
+        regions[i].address = NOT_VALID;
+        regions[i].size = NOT_VALID;
+    }
+
+    FILE* fp;
+    char buf[bufSize];
+    if (argc != 3)
+    {
+        fprintf(stderr, "Usage: %s <annotated regions> <memory refs>\n", argv[0]);
+        return 1;
+    }
+    if ((fp = fopen(argv[1], "r")) == NULL)
+    {
+        perror("fopen source-file");
+        return 1;
+    }
+
+    while (fgets(buf, sizeof(buf), fp) != NULL)
+    {
+        buf[strlen(buf) - 1] = '\0'; // eat the newline fgets() stores
+        //printf("Reading-> %s\n", buf);
+        sscanf(buf,"LABEL:%s g:%d nc:%d addr:%lx size:%d\n",label,&granularity,&n_controllers,&address,&size);
+
+        printf("Analysing region [%s](%lx-%lx,%d)\n",label,address,address+size,size);
+
+        strcpy(regions[current_region].label,label);
+        regions[current_region].address = address;
+        regions[current_region].size = size;
+        current_region++;
+    }
+    total_regions = current_region;
+    fclose(fp);
+
+    if ((fp = fopen(argv[2], "r")) == NULL)
+    {
+        perror("Cannot open annotated file");
+        return 1;
+    }
+
+    while (fgets(buf, sizeof(buf), fp) != NULL)
+    {
+        buf[strlen(buf) - 1] = '\0'; // eat the newline fgets() stores
+        //printf("Reading-> %s\n", buf);
+        sscanf(buf,"DRAM addr:%lx size:%d src:%d dst:%d\n",&address,&size,&src,&dst);
+        //printf("Reading access addr:%lx size:%d src:%d dst:%d\n",address,size,src,dst);
+
+        for (i = 0; i <total_regions;i++)
+        {
+            int region_start = regions[i].address;
+            int region_end = regions[i].address+regions[i].size;
+            if ( address >= region_start && address <= region_end)
+            {
+                communications[src][dst]++;
+            }
+        }
+
+    }
+    fclose(fp);
+
+    printf("----------------------------------------\n");
+    printf(" approximate communication list \n");
+    printf("----------------------------------------\n");
+   
+    for (i=0;i<MAX_THREADS;i++)
+        for(j=0;j<MAX_THREADS;j++)
+        {
+            // assuming send size of cacheline 64 bytes
+            if (communications[i][j]!=0)
+                printf("src: %d dst: %d occurences: %d total bytes: %d\n",i,j,communications[i][j],communications[i][j]*64);
+        }
+
+}
